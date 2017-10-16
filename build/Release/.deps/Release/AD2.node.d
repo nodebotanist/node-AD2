3810304f67b2efbc64b1619fb1810707629b11c2
Kas Perch <the@nodebotani.st>
2017-10-16 16:19:38 -0500

    Cleanup

--- build/Release/.deps/Release/AD2.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/AD2.node := c++ -bundle -undefined dynamic_lookup -Wl,-no_pie -Wl,-search_paths_first -mmacosx-version-min=10.7 -arch x86_64 -L./Release -stdlib=libc++  -o Release/AD2.node Release/obj.target/AD2/AD2.o 
